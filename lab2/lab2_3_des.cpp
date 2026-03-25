6320c920361bb8d7271dee51fef764e6bdcb5ba7
HuyGia <huy.hongha76@gmail.com>
2026-03-25 20:27:04 +0700

    Refactor DES key and plaintext handling

--- lab2/lab2_3_des.cpp
+++ lab2/lab2_3_des.cpp
@@ -10,22 +10,25 @@ string to_bin(unsigned char* d){
 }
 
 int main(){
-    DES_cblock key="87654321";
+    unsigned char key_raw[9] = "23520688"; 
     DES_key_schedule sch;
-    DES_set_key_unchecked(&key,&sch);
+    
+    DES_set_key_unchecked((DES_cblock*)key_raw, &sch);
 
-    unsigned char p1[8]="STAYHOME";
-    unsigned char p2[8]="STAYHOMA";
-    unsigned char c1[8],c2[8];
+    unsigned char p1[9] = "STAYHOME";
+    unsigned char p2[9] = "STAYHOMA";
+    unsigned char c1[8], c2[8];
 
-    DES_ecb_encrypt((DES_cblock*)p1,(DES_cblock*)c1,&sch,DES_ENCRYPT);
-    DES_ecb_encrypt((DES_cblock*)p2,(DES_cblock*)c2,&sch,DES_ENCRYPT);
+    DES_ecb_encrypt((DES_cblock*)p1, (DES_cblock*)c1, &sch, DES_ENCRYPT);
+    DES_ecb_encrypt((DES_cblock*)p2, (DES_cblock*)c2, &sch, DES_ENCRYPT);
 
-    string b1=to_bin(c1),b2=to_bin(c2);
+    string b1=to_bin(c1), b2=to_bin(c2);
 
     int diff=0;
     for(int i=0;i<64;i++) if(b1[i]!=b2[i]) diff++;
 
-    cout<<"Diff bits: "<<diff<<endl;
-    cout<<"Percent: "<<diff/64.0*100<<"%"<<endl;
+    cout << "Diff bits: " << diff << endl;
+    cout << "Percent: " << diff/64.0*100 << "%" << endl;
+    
+    return 0;
 }
