8142ed7a501b772a56f5fe16f10c08660b9d590b
KhangHuynh <huynhvykhang1234@gmail.com>
2026-03-25 19:44:59 +0700

    update source to print CBC and ECB in terminal

--- lab2/lab2_2_aes.cpp
+++ lab2/lab2_2_aes.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <openssl/aes.h>
+#include <cstring>
 using namespace std;
 
 int main(){
     unsigned char key[16]="1234567890123456";
     unsigned char pt[32]="UIT_LAB_UIT_LAB_UIT_LAB_UIT_LAB_";
-    unsigned char out[32];
+    unsigned char out_ecb[32], out_cbc[32];
+    unsigned char iv[16] = {0}; 
 
     AES_KEY enc;
-    AES_set_encrypt_key(key,128,&enc);
+    AES_set_encrypt_key(key, 128, &enc);
 
-    for(int i=0;i<32;i+=16)
-        AES_ecb_encrypt(pt+i,out+i,&enc,AES_ENCRYPT);
+    for(int i=0; i<32; i+=16)
+        AES_ecb_encrypt(pt+i, out_ecb+i, &enc, AES_ENCRYPT);
 
-    cout<<"ECB: ";
-    for(int i=0;i<32;i++) printf("%02x",out[i]);
+    unsigned char tmp_iv[16]; 
+    memcpy(tmp_iv, iv, 16);
+    AES_cbc_encrypt(pt, out_cbc, 32, &enc, tmp_iv, AES_ENCRYPT);
+
+    cout << "ECB: ";
+    for(int i=0; i<32; i++) printf("%02x", out_ecb[i]);
+    
+    cout << "\nCBC: ";
+    for(int i=0; i<32; i++) printf("%02x", out_cbc[i]);
+    cout << endl;
+
+    return 0;
 }
